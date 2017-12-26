fac5b2069612a6bd05f73260af65914b262f447e
Juan Montoya <jcmontoya@gmail.com>
2017-12-26 16:22:45 +0000

    functioning calc, but is with eval method.

--- calculator.js
+++ calculator.js
@@ -1,19 +1,3 @@
-// var display = document.getElementById('display');
-// var result = document.getElementById('result');
-
-// function toDisplay(x){
-//     display.value+=x;
-//     if (x==='c'){
-//         display.value='';
-//     }
-// }
-
-// function equal(){
-//     var runningTotal = 0;
-//     runningTotal += parseInt(display.value, 10);
-//     result.value = display.value;
-//     console.log(result.value);
-//     if(display.value===''){
-//         result.value = '';
-//     }
+// function answer(){
+    
 // }
