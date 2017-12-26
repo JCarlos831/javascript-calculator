3875069a7a6b614d9938ce636260bff41c7e8779
Juan Montoya <jcmontoya@gmail.com>
2017-12-26 01:12:34 +0000

    Commencing javascript

--- calculator.js
+++ calculator.js
@@ -1,65 +1,8 @@
-$(document).ready(function(){
-    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = 1;
-    });
-    $('#button2').click(function(){
-    document.getElementById('calculations').innerHTML = 2;
-    });    
-    $('#button3').click(function(){
-    document.getElementById('calculations').innerHTML = 3;
-    });    
-    $('#button4').click(function(){
-    document.getElementById('calculations').innerHTML = 4;
-    });    
-    $('#button5').click(function(){
-    document.getElementById('calculations').innerHTML = 5;
-    });    
-    $('#button6').click(function(){
-    document.getElementById('calculations').innerHTML = 6;
-    });    
-    $('#button7').click(function(){
-    document.getElementById('calculations').innerHTML = 7;
-    });    
-    $('#button8').click(function(){
-    document.getElementById('calculations').innerHTML = 8;
-    });    
-    $('#button9').click(function(){
-    document.getElementById('calculations').innerHTML = 9;
-    });    
-    $('#button0').click(function(){
-    document.getElementById('calculations').innerHTML = 0;
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });   
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });    
-    $('#button1').click(function(){
-    document.getElementById('calculations').innerHTML = '1';
-    });
-    
-    
-});
+var box = document.getElementById('display');
+
+function toDisplay(x){
+    box.value+=x;
+    if (x==='c'){
+        box.value='';
+    }
+}
