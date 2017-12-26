3782bea50fcf80a55453eeab1013d70419e7c51b
Juan Montoya <jcmontoya@gmail.com>
2017-12-26 15:39:07 +0000

    Calculator functions with eval method.  Need to do without eval.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
-var box = document.getElementById('display');
+// var display = document.getElementById('display');
+// var result = document.getElementById('result');
 
-function toDisplay(x){
-    box.value+=x;
-    if (x==='c'){
-        box.value='';
-    }
-}
+// function toDisplay(x){
+//     display.value+=x;
+//     if (x==='c'){
+//         display.value='';
+//     }
+// }
+
+// function equal(){
+//     var runningTotal = 0;
+//     runningTotal += parseInt(display.value, 10);
+//     result.value = display.value;
+//     console.log(result.value);
+//     if(display.value===''){
+//         result.value = '';
+//     }
+// }
