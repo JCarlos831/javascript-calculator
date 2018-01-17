c9b056b10cd0b8b2bd46c99727bff4240cccbd5a
Juan Montoya <jcmontoya@gmail.com>
2018-01-17 04:28:18 +0000

    calculations work.  css needs work.

--- calculator.js
+++ calculator.js
@@ -1,47 +1,41 @@
 /* global $ */
 
 $(document).ready(function(){
-	var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
-            }
-        } 
-    };
+
 	var number = "";
-    var newnumber = "";
+    var newNumber = "";
     var operator = "";
     var totaldiv = $("#total");
     totaldiv.text("0");
     
-    $("#numbers a").not("#clear, #clearAll").click(function(){
-        number += $(this).html();
-        totaldiv.text(number);
-        testNumLength(number);
+    $("#numbers a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
     });
-    
     $("#operators a").not("#equals").click(function(){
-        operator = $("#button").text();
-        newnumber = number;
-        number = "";
-        totaldiv.text("0");
+		operator = $(this).text();
+		newNumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clear,#clearAll").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if (document.getElementById() === "clearAll") {
+			newNumber = "";
+		}
     });
 
-    $("#clear, #clearAll").click(function(){
-       number = "";
-       totaldiv.text("0");
-       if(document.getElementById() === "clearAll") {
-           newnumber = "";
-       }
-    });
-
-    $("#equals a").click(function(){
-        if(operator === "+") {
-            parseInt(newnumber, 10);
-            parseInt(number, 10);
-            
-        }
+    $("#equals").click(function(){
+    	if (operator === "+"){
+    		number = (parseInt(number, 10) + parseInt(newNumber,10)).toString(10);
+    	} else if (operator === "-"){
+    		number = (parseInt(newNumber, 10) - parseInt(number,10)).toString(10);
+    	} else if (operator === "/"){
+    		number = (parseInt(newNumber, 10) / parseInt(number,10)).toString(10);
+    	} else if (operator === "*"){
+    		number = (parseInt(newNumber, 10) * parseInt(number,10)).toString(10);
+    	}
+    	totaldiv.text(number);
     });
 });
