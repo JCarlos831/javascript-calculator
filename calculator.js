4325443e4282201f31aa003695bba12453a6ea8d
Juan Montoya <jcmontoya@gmail.com>
2018-01-20 15:16:20 +0000

    clear button not working

--- calculator.js
+++ calculator.js
@@ -3,3 +3,15 @@ var display = document.getElementById("display");
 function toDisplay (x) {
     display.value += x;
 }
+
+function clearDisplay() {
+    display.value="";
+}
+
+function clear() {
+    var num1 = display.value;
+    var len = num1.length-1;
+    console.log(len);
+    var num2 = num1.substring(0, len);
+    display.value = num2;
+}
